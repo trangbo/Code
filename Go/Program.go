f68d6838325305a2cbd7b9aa55783f1e3a3ca149
trangbo <trangbo0318@outlook.com>
2021-12-15 23:16:42 +0800

    commit

--- Go/Program.go
+++ Go/Program.go
@@ -1,15 +1,41 @@
-// Echo2 prints its command-line arguments
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"time"
-	"strings"
 )
 
 func main() {
-	start := time.Now();
-	fmt.Println(strings.Join(os.Args[1:], " "))
-	fmt.Println(time.Since(start))
+	counts := make(map[string]int)
+	countsFiles := make(map[string]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, countsFiles)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, countsFiles)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\t%s\n", n, line, countsFiles[line])
+		}
+	}
+}
+func countLines(f *os.File, counts map[string]int, countsFiles map[string]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+		if counts[input.Text()] > 1 {
+			countsFiles[input.Text()] = f.Name()
+		}
+	}
+	// NOTE: ignoring potential errors from input.Err()
 }
